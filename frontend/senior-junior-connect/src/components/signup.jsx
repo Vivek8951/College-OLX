fa3469f5a01f36dce54d1f094cc06294769f31ab
Chirag Khati <145248783+potatotify@users.noreply.github.com>
2025-03-26 10:24:58 +0530

    Update signup.jsx

--- frontend/senior-junior-connect/src/components/signup.jsx
+++ frontend/senior-junior-connect/src/components/signup.jsx
@@ -23,7 +23,7 @@ export default function Register() {
     e.preventDefault(); 
 
     try {
-      const response = await fetch("http://localhost:8080/signup", {
+      const response = await fetch("https://college-olx-backend.onrender.com/signup", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
