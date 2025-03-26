95963b5a7e483124f17f3b782a5da107edec24c7
Chirag Khati <145248783+potatotify@users.noreply.github.com>
2025-03-26 10:22:28 +0530

    Update login.jsx

--- frontend/senior-junior-connect/src/components/login.jsx
+++ frontend/senior-junior-connect/src/components/login.jsx
@@ -21,7 +21,7 @@ export default function Login() {
     e.preventDefault(); 
   
     try {
-      const response = await fetch("http://localhost:8080/login", {
+      const response = await fetch("https://college-olx-backend.onrender.com/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
